3084e2215d3d668db089cae322aa42b501a11680
nouriach <nathan.ouriach@deptagency.com>
2022-07-02 13:32:51 -0400

    Add Domain models

---
+++ OTB.HolidaySearch.Web/Domain/Models/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace OTB.HolidaySearch.Web.Domain.Models
+{
+   public class Flight
+   {
+      public int Id { get; set; }
+      public string? Airline { get; set; }
+      public string? From { get; set; }
+      public string? To { get; set; }
+      public string? Price { get; set; }
+      public DateTime DepartureDate { get; set; }
+   }
+}
---
+++ OTB.HolidaySearch.Web/Domain/Models/Hotel.cs
@@ -0,0 +1,12 @@
+﻿namespace OTB.HolidaySearch.Web.Domain.Models
+{
+   public class Hotel
+   {
+      public int Id { get; set; }
+      public DateTime ArrivalData { get; set; }
+      public string? PricePerNight { get; set; }
+      public IEnumerable<string>? LocalAirports { get; set; }
+      public int Nights { get; set; }
+
+   }
+}
