b4600988463134f03c11d85a34fe5c697ac92fa5
nouriach <nathan.ouriach@deptagency.com>
2022-07-04 05:18:31 -0400

    Use StreamReader to deserialize JSON data
    - Return all hotels
    - Return all flights
    - Make DatabaseService tests pass

--- OTB.HolidaySearch.Web/Infrastructure/Services/DatabaseService.cs
+++ OTB.HolidaySearch.Web/Infrastructure/Services/DatabaseService.cs
@@ -1,4 +1,6 @@
-﻿using OTB.HolidaySearch.Web.Applications.Common.Interfaces;
+﻿using Newtonsoft.Json;
+using OTB.HolidaySearch.Web.Applications.Common.Interfaces;
+using OTB.HolidaySearch.Web.Applications.Mediator.Flight.Responses;
 using OTB.HolidaySearch.Web.Applications.Mediator.Flights.Queries;
 using OTB.HolidaySearch.Web.Applications.Mediator.Flights.Responses;
 using OTB.HolidaySearch.Web.Applications.Mediator.Hotels.Queries;
@@ -11,12 +13,23 @@ namespace OTB.HolidaySearch.Web.Infrastructure.Services
       public FlightsResult GetAllFlights(GetAllFlightsQuery query)
       {
          var flights = new FlightsResult();
+         using (StreamReader reader = new StreamReader("C:/Users/nathan.ouriach/source/repos/Misc/TechTests/OnTheBeach/OTB.HolidaySearch/OTB.HolidaySearch.Web/Infrastructure/Data/flightData.json"))
+         {
+            var json = reader.ReadToEnd();
+            flights = JsonConvert.DeserializeObject<FlightsResult> (json);
+         }
+
          return flights;
       }
 
       public HotelsResult GetAllHotels(GetAllHotelsQuery query)
       {
          var hotels = new HotelsResult();
+         using (StreamReader reader = new StreamReader("C:/Users/nathan.ouriach/source/repos/Misc/TechTests/OnTheBeach/OTB.HolidaySearch/OTB.HolidaySearch.Web/Infrastructure/Data/hotelData.json"))
+         {
+            var json = reader.ReadToEnd();
+            hotels = JsonConvert.DeserializeObject<HotelsResult>(json);
+         }
          return hotels;
       }
    }
