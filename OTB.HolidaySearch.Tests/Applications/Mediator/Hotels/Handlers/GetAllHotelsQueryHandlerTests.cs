df429c1f4c8a1273e003db4460cfa25a17ef52aa
nouriach <nathan.ouriach@deptagency.com>
2022-07-04 05:17:08 -0400

    Write failing DatabaseService test & make project compile

--- OTB.HolidaySearch.Tests/Applications/Mediator/Hotels/Handlers/GetAllHotelsQueryHandlerTests.cs
+++ OTB.HolidaySearch.Tests/Applications/Mediator/Hotels/Handlers/GetAllHotelsQueryHandlerTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
 using OTB.HolidaySearch.Web.Applications.Common.Interfaces;
 using OTB.HolidaySearch.Web.Applications.Mediator.Hotel.Responses;
---
+++ OTB.HolidaySearch.Tests/Infrastructure/Services/DatabaseService.cs
@@ -0,0 +1,49 @@
+﻿using OTB.HolidaySearch.Web.Applications.Common.Interfaces;
+using OTB.HolidaySearch.Web.Applications.Mediator.Flights.Queries;
+using OTB.HolidaySearch.Web.Applications.Mediator.Hotels.Queries;
+using OTB.HolidaySearch.Web.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTB.HolidaySearch.Tests.Infrastructure.Services
+{
+   public class DatabaseServiceTests
+   {
+      private IDatabaseService _databaseService;
+
+      [SetUp]
+      public void Setup()
+      {
+         _databaseService = new DatabaseService();
+      }
+
+      [Test]
+      public void DatabaseServiceTest_ReturnsAllHotels()
+      {
+         // arrange
+         var query = new GetAllHotelsQuery();
+
+         // act
+         var actual = _databaseService.GetAllHotels(query);
+
+         // assert
+         Assert.IsNotNull(actual);
+      }
+
+      [Test]
+      public void DatabaseServiceTest_ReturnsAllFlights()
+      {
+         // arrange
+         var query = new GetAllFlightsQuery();
+
+         // act
+         var actual = _databaseService.GetAllFlights(query);
+
+         // assert
+         Assert.IsNotNull(actual);
+      }
+   }
+}
