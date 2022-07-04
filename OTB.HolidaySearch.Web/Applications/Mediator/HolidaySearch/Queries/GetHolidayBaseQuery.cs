067334462c9dd4fedcb6b3e76d94a26461d4d881
nouriach <nathan.ouriach@deptagency.com>
2022-07-04 15:38:02 -0400

    Add base query to Holiday requests
    - allow queries to inherit identical parameters
    - allow queries to extend requests with their own
    unique params

---
+++ OTB.HolidaySearch.Web/Applications/Mediator/HolidaySearch/Queries/GetHolidayBaseQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace OTB.HolidaySearch.Web.Applications.Mediator.HolidaySearch.Queries
+{
+   public class GetHolidayBaseQuery
+   {
+      public string TravellingTo { get; set; }
+      public DateTime DepartureDate { get; set; }
+      public int Duration { get; set; }
+   }
+}
--- OTB.HolidaySearch.Web/Applications/Mediator/HolidaySearch/Queries/GetHolidayByCityQuery.cs
+++ OTB.HolidaySearch.Web/Applications/Mediator/HolidaySearch/Queries/GetHolidayByCityQuery.cs
@@ -3,8 +3,8 @@ using OTB.HolidaySearch.Web.Applications.Mediator.HolidaySearch.Responses;
 
 namespace OTB.HolidaySearch.Web.Applications.Mediator.HolidaySearch.Queries
 {
-   public class GetHolidayQuery : GetHolidayBaseQuery, IRequest<HolidayResult>
+   public class GetHolidayByCityQuery : GetHolidayBaseQuery, IRequest<HolidayResult>
    {
-      public string DepartingFrom { get; set; }
+      public string DepartingFromCity { get; set; }
    }
 }
