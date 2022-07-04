21b37715765982f75294c84baf746a7aa17061d9
nouriach <nathan.ouriach@deptagency.com>
2022-07-04 05:17:41 -0400

    Install NewtonSoft & add json attributes to props

--- OTB.HolidaySearch.Web/Applications/Mediator/Flight/Responses/FlightResult.cs
+++ OTB.HolidaySearch.Web/Applications/Mediator/Flight/Responses/FlightResult.cs
@@ -1,11 +1,18 @@
-﻿namespace OTB.HolidaySearch.Web.Applications.Mediator.Flight.Responses
+﻿using Newtonsoft.Json;
+
+namespace OTB.HolidaySearch.Web.Applications.Mediator.Flight.Responses
 {
    public class FlightResult
    {
+      [JsonProperty("airline")]
       public string? Airline { get; set; }
+      [JsonProperty("from")]
       public string? From { get; set; }
+      [JsonProperty("to")]
       public string? To { get; set; }
+      [JsonProperty("price")]
       public string? Price { get; set; }
+      [JsonProperty("departure_date")]
       public DateTime DepartureDate { get; set; }
    }
 }
--- OTB.HolidaySearch.Web/Applications/Mediator/Hotel/Responses/HotelResult.cs
+++ OTB.HolidaySearch.Web/Applications/Mediator/Hotel/Responses/HotelResult.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace OTB.HolidaySearch.Web.Applications.Mediator.Hotel.Responses
 {
    public class HotelResult
    {
+      [JsonProperty("name")]
       public string? Name { get; set; }
+      [JsonProperty("arrival_date")]
       public DateTime ArrivalDate { get; set; }
+      [JsonProperty("price_per_night")]
       public int PricePerNight { get; set; }
+      [JsonProperty("local_airports")]
       public IEnumerable<string>? LocalAirports { get; set; }
+      [JsonProperty("nights")]
       public int Nights { get; set; }
    }
 }
