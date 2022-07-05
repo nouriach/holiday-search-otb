9c1adecfbf0e4071057b3d7f47d50917df480724
nouriach <nathan.ouriach@deptagency.com>
2022-07-05 08:46:09 -0400

    Add query for getting flight by any departure

---
+++ OTB.HolidaySearch.Web/Applications/Mediator/Flight/Queries/GetFlightByAnyDepartureCityQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using OTB.HolidaySearch.Web.Applications.Mediator.Flight.Responses;
+
+namespace OTB.HolidaySearch.Web.Applications.Mediator.Flight.Queries
+{
+   public class GetFlightByAnyDepartureCityQuery : IRequest<FlightResult>
+   {
+      public string TravellingTo { get; set; }
+      public DateTime DepartureDate { get; set; }
+   }
+}
